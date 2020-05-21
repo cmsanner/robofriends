5d71f2ba9ef44e374ca5d4a83903237c4e3aa2d2
cmsanner <craftbloggerfozzie@gmail.com>
2020-05-21 12:58:23 -0500

    change http to https for user data

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
     componentDidMount(){
         //http://jsonplaceholder.typicode.com/users
-        fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => { return response.json(); })
             .then(users => {this.setState({robots: users}) })
     }
