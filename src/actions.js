9dfad91a0abaac1e00d2395be11f3fe9e81b95b7
cmsanner <craftbloggerfozzie@gmail.com>
2020-07-28 16:26:50 -0500

    add redux-logger for debugging

--- src/actions.js
+++ src/actions.js
@@ -2,7 +2,6 @@ import {CHANGE_SEARCH_FIELD} from './constants.js';
 
 //search field will recieve an input of text 
 export const setSearchField = (text) => {
-    console.log('#5 in actions setSearchField: ',text);
     return {
         type: 'CHANGE_SEARCH_FIELD',
         payload: text
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import {createLogger} from 'redux-logger';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
+const logger = createLogger();
 // const store = createStore(rootReducer); <---- if you had multiple reducers
-const store = createStore(searchRobots);
+const store = createStore(searchRobots,applyMiddleware(logger));
 
 //1) now we can pass the store to our App
 // ReactDOM.render(<App store={store}/> , document.getElementById('root'));
